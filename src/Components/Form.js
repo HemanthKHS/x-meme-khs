a3a6e75862c71aff3cb2d3a268c4fbd414ad5653
HemanthKHS <59533188+HemanthKHS@users.noreply.github.com>
2021-02-16 17:36:09 +0530

    Update Form.js

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -39,14 +39,14 @@ import React, { Component } from "react";
                 body: JSON.stringify(this.state)
             };
             fetch('https://x-meme-khs.herokuapp.com/memes', requestOptions)
-            
+            .then(window.location.reload();)
             this.setState({
                 "name": "",
                 "caption": "",
                 "url": "",
             })
     
-                window.location.reload();
+            
              
 
          event.preventDefault()
@@ -78,68 +78,3 @@ import React, { Component } from "react";
 export default Form
 
 
-// class Form extends Component{
-//     constructor(props) {
-//         super(props)
-
-//         this.state = {
-//             name: "",
-//             caption: "",
-//             url: ""
-//         }
-//         this.handleSubmit.bind(this)
-//     }
-    
-//     namehandler = (event) => {
-//         this.setState({
-//             name: event.target.value
-//         })
-//     }
-//     captionhandler = (event) => {
-//         this.setState({
-//             caption: event.target.value
-//         })
-//     }
-//     urlhandler = (event) => {
-//         this.setState({
-//             url: event.target.value
-//         })
-//     }
-
-//     handleSubmit = (event) => {
-//         console.log("hello")
-//         alert(`${this.state.firstName} ${this.state.lastName}  Registered Successfully !!!!`)
-//         console.log(this.state);
-//         this.setState({
-//             name: "",
-//             caption: "",
-//             url: "",
-
-//         })
-//      event.preventDefault()
-        
-//     }
-
-
-// render(){
-//     return(
-//     <div>
-//         <form action="">
-//             <label htmlFor="name">Name :</label>
-//             <input type="text" value={this.state.name} onChange={this.namehandler} placeholder="name..." /><br/>
-
-//             <label htmlFor="caption">Caption :</label>
-//             <input type="text" value={this.state.caption} onChange={this.captionhandler} placeholder="caption..." id="caption" name="caption"/><br/>
-            
-//             <label htmlFor="url">URL :</label>
-//             <input type="url" value={this.state.url} onChange={this.urlhandler} placeholder="url..." id="url" name="url" /><br/>
-
-//             <input type="submit" value="submit"/>
-
-//         </form>
-//     </div>
-// )
-//     }
-// }
-
-// export default Form
